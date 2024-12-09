14c45b1b4f446cfdee7bb92241414edd7e624e02
zandy <zandy@zandy.xyz>
2024-12-09 03:02:13 -0600

    Horribly Slow Day 9 Part 2

--- src/bin/day_9.rs
+++ src/bin/day_9.rs
@@ -12,11 +12,10 @@ const ASCII_ZERO: u8 = 48;
 fn main() -> Result<(), Box<dyn Error>> {
     let bytes = ByteIterator::new("files/day_9_input.txt").unwrap();
     let disk = parse(bytes);
+    let disk_backup = disk.clone();
     let part_1 = solve_part_1(disk);
     println!("The checksum of the fragmented disk is:\n{}", part_1);
-    let bytes = ByteIterator::new("files/day_9_input.txt").unwrap();
-    let disk_backup = parse_sillymode(bytes);
-    let (part_2, changed_disk) = solve_part_2(disk_backup);
+    let part_2 = solve_part_2(disk_backup);
     println!("The checksum of the sorted but unfragmented disk is:\n{}", part_2);
 
     Ok(())
@@ -54,59 +53,6 @@ pub fn parse(bytes: ByteIterator) -> Vec<Num> {
     disk
 }
 
-pub fn parse_sillymode(bytes: ByteIterator) -> Vec<Block> {
-    let mut disk = Vec::with_capacity(bytes.len());
-    let mut is_file = true;
-    let mut file_id: Num = 0;
-    'exit: for byte in bytes {
-        if byte.is_err() {
-            break 'exit;
-        }
-        let byte = byte.unwrap();
-        if byte < 48 {
-            break 'exit;
-        }
-        let size = byte - ASCII_ZERO;
-        let n = match is_file {
-            true => {
-                is_file = false;
-                let temp = file_id;
-                file_id += 1;
-                temp
-            }
-            false => {
-                is_file = true;
-                Num::MAX
-            },
-        };
-        disk.push(Block {
-            id: n,
-            size,
-        });
-    }
-    for block in &disk {
-        print!("{}", block)
-    }
-    println!();
-    disk
-}
-
-struct Block {
-    id: Num,
-    size: u8,
-}
-
-impl Display for Block {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        if self.id == Num::MAX {
-            write!(f, "[{}; _]", self.size)
-        } else {
-            let n = (self.id as u8 + 48) as char;
-            write!(f, "[{};{}]", self.size, n)
-        }
-    }
-}
-
 pub fn solve_part_1(mut disk: Vec<Num>) -> u64 {
     let mut front: usize = 0;
     let mut back: usize = disk.len() - 1;
@@ -135,41 +81,75 @@ pub fn solve_part_1(mut disk: Vec<Num>) -> u64 {
     checksum(&disk)
 }
 
-pub fn solve_part_2(mut disk: Vec<Block>) -> (u64, Vec<Block>) {
+pub fn solve_part_2(mut disk: Vec<Num>) -> u64 {
+    // println!("{:?}", disk);
     let mut back: usize = disk.len() - 1;
-    let limit: usize = disk.len() * 2;
+    let limit: usize = disk.len();
     let mut loops: usize = 0;
-
-    'quit: while back > 0 && loops < limit {
+    'quit: while back > 0 {
         loops += 1;
 
-        while disk[back].id == Num::MAX {
+        while disk[back] == Num::MAX {
             back -= 1;
-            if back == 0 {
+            if back <= 0 {
+                // println!("A");
                 break 'quit;
             }
         }
-        let mut front: usize = 0;
-        while front < disk.len() && !(disk[front].id == Num::MAX && disk[front].size >= disk[back].size) {
+        let current_id = disk[back];
+        let mut back_start = back;
+        'found: while disk[back_start] == current_id {
+            back_start -= 1;
+            if back_start <= 0 {
+                // println!("B");
+                break 'quit;
+            }
+            if disk[back_start] != current_id {
+                back_start += 1;
+                break 'found;
+            }
+        }
+        /*print!("[");*/
+        let range_to_move = &disk[back_start..=back];
+        let required_length = range_to_move.len();
+        /*for i in back_start..=back {
+            print!("{}", disk[i] as u32)
+        }
+        println!("] ({})", required_length);*/
+
+        let mut front = 0;
+        let mut front_end = 0;
+        'outer: while front + required_length < back_start {
+            while disk[front] != Num::MAX && front + 1 < back_start {
+                front += 1;
+                if disk[front] == Num::MAX {
+                    front_end = front;
+                    while disk[front_end] == Num::MAX {
+                        front_end += 1;
+                    }
+                    if front_end - front >= required_length {
+                        break 'outer;
+                    }
+                }
+            }
             front += 1;
         }
-        if front < disk.len() {
-            println!("swapping");
-            if disk[front].size > disk[back].size {
-                disk.insert(front + 1, Block {
-                    id: Num::MAX,
-                    size: disk[front].size - disk[back].size,
-                });
-                back += 1;
+
+        // println!("{} -> {}", front, front_end);
+        if front_end >= front && front_end - front >= required_length {
+            // println!("SWAP");
+            for i in 0..required_length {
+                disk[front + i] = disk[back_start + i];
+                disk[back_start + i] = Num::MAX;
             }
-            disk[front].id = disk[back].id;
-            disk[front].size = disk[back].size;
-            disk[back].id = Num::MAX;
         }
+
+        //debug_print(&disk);
+        back -= required_length;
     }
+    // println!("{}/{}", loops, limit);
 
-
-    (0, disk)
+    checksum(&disk)
 }
 
 pub fn checksum(disk: &Vec<Num>) -> u64 {
@@ -208,25 +188,17 @@ pub fn debug_string(disk: &Vec<Num>) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, parse_sillymode, solve_part_1, solve_part_2};
+    use crate::{parse, solve_part_1, solve_part_2};
     use crate::util::bytewise::ByteIterator;
 
     #[test]
     fn tiny_input() {
         let bytes = ByteIterator::new("files/day_9_tiny.txt").unwrap();
         let disk = parse(bytes);
+        let disk_backup = disk.clone();
         let part_1 = solve_part_1(disk);
-        let bytes = ByteIterator::new("files/day_9_tiny.txt").unwrap();
-        let disk_backup = parse_sillymode(bytes);
-        for block in &disk_backup {
-            print!("{}", block);
-        }
         println!();
-        let (checksum, changed_disk) = solve_part_2(disk_backup);
-        for block in changed_disk {
-            print!("{}", block);
-        }
-        println!();
+        let part_2 = solve_part_2(disk_backup);
         let steps = [
             "0..111....22222",
             "02.111....2222.",
@@ -242,18 +214,10 @@ mod tests {
         "2333133121414131402";
         let bytes = ByteIterator::new("files/day_9_small.txt").unwrap();
         let disk = parse(bytes);
+        let disk_backup = disk.clone();
         let part_1 = solve_part_1(disk);
-        let bytes = ByteIterator::new("files/day_9_small.txt").unwrap();
-        let disk_backup = parse_sillymode(bytes);
-        for block in &disk_backup {
-            print!("{}", block);
-        }
-        println!();
-        let (checksum, changed_disk) = solve_part_2(disk_backup);
-        for block in changed_disk {
-            print!("{}", block);
-        }
         assert_eq!(part_1, 1928);
+        let part_2 = solve_part_2(disk_backup);
     }
 
 }
